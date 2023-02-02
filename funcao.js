a1654e0a44d60f7ba77a42af3baff9dd4c7c7eda
felpsGSantos <felipe.gomes04@hotmail.com.br>
2023-02-02 10:59:45 -0300

    funcoes.js

---
+++ funcao.js
@@ -0,0 +1,88 @@
+
+
+
+
+//Arrow function
+//Ela consegur simplificar a função ao criar as função e denominar um certo valor 
+//dentro do parâmetro e atribuir um calculo ao lado com "+>" e na linha de baixo
+// estou atrubuindo o valor dos números 
+    
+/*   let myFunction = (x,y) => x - y
+
+    console.log(myFunction(1,3))*/ 
+
+
+//Função com parâmetro
+
+/*    function sum(x,y){
+    return x + y
+}
+
+console.log(sum(3,6))*/
+
+// Básicamente a função "return" é uma declaração que finaliza a execução de uma função
+//retornando um determinado valor. é executado tudo que vem antes da função retorno
+//mas essa função não executa quaisquer outra execução que vem depois da função
+/*function Teste(a, b){
+    return console.log("Olá")
+    console.log(a + b) //código inalcançável!!
+}
+Teste(3,2)*/ //imprime Olá
+
+//  você percebe que o resto da execução de esta´depois do return muda de tonação
+
+//Função Anônima
+//Não é necessário atribuir um nome
+
+/*var square = function(numero) {return numero * numero}
+    
+console.log(square(4))*/
+
+// Função com parâmetro
+
+/*let nameLetters = (name) => name.length
+
+console.log(nameLetters('Helinho'))*/
+
+/*let centimetro = function(centimetro) {return centimetro /100}
+
+console.log(centimetro(5))*/
+
+/*let conversao = (r,d) => r * 5.02
+
+console.log(conversao(2.00))*/
+
+function maior(a,b){
+
+    
+
+if (a > b){
+    console.log(`O número que voce digitou foi ${a} e ${b} e o ${a} é o maior `)
+}
+else{
+    console.log(`O número que voce digitou foi ${a} e ${b} e o ${b} é o maior  `)
+}
+
+}
+
+maior(10,4)
+
+ 
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
